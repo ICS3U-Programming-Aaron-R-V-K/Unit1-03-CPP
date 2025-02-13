db556f68dbe03b391f7dab7afc15e86925df6877
Aaron Rivelino Vargas Kononenko <aaron.rivelino.vargas-kononenko@stu.ocsb.ca>
2025-02-13 03:32:02 +0000

    Added helloWorldStyle.cpp

---
+++ helloWorldStyle.cpp
@@ -0,0 +1,11 @@
+//  Copyright (2025) Aaron Rivelino All rights reserved.
+//  Created By : Aaron
+//  Date : 2025 - 02 - 12
+//  The code is a Hello World program, that displays sentences
+
+#include<iostream>
+
+int main() {
+    std::cout << "Hello, World with style" << std::endl;
+    std::cout << "I like oranges" << std::endl;
+}
